c265638c7466d64107b513b6794062a393fe2417
Jezzjezz <32496635+Jezzjezz@users.noreply.github.com>
2017-11-21 16:24:58 -0600

    App.js Part 4
    Part 4 of Ravenous

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
+import Yelp from './util/Yelp';
 
-const business ={
-	imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-const x_business = [x_business,x_business,x_business,x_business,x_business,x_business];
+let business = {};
+
+let businesses = [];
 
 class App extends Component {
-
+	constructor(props){
+	super(props);
+	this.state = {businesses: []};
+	this.searchYelp.bind(this);
+}
 	searchYelp(term, location, sortBy){
-		console.log(`Searching Yelp with ${term}, ${location}, ${sortBy} `);
+Yelp.search(term, location, sortBy).then(businesses => {
+	this.setState({businesses: businesses});
+});
 	}
   render() {
     return (
@@ -28,7 +26,6 @@ class App extends Component {
   		<h1>ravenous</h1>
   		<SearchBar searchYelp={this.searchYelp}/>
   		<BusinessList businesses={this.state.businesses}/>
-			<h1>END</h1>
 			</div>
   );
   }
