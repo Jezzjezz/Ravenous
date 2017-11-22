077857d3edde0ed30c8c8e9cf1ae849c5cadee9a
Jezzjezz <32496635+Jezzjezz@users.noreply.github.com>
2017-11-22 13:18:46 -0600

    got rid of {} lines 27 28

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ Yelp.search(term, location, sortBy).then(businesses => {
     return (
       <div className ="App">
   		<h1>ravenous</h1>
-  		{<SearchBar searchYelp={this.searchYelp}/>}
-  		{<BusinessList businesses={this.state.businesses}/>}
+  		<SearchBar searchYelp={this.searchYelp}/>
+  		<BusinessList businesses={this.state.businesses}/>
 			</div>
   );
   }
