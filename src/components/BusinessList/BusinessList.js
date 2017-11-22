01db91078f2d6eb94cede12cc0c36e4358c73f7b
Jezzjezz <32496635+Jezzjezz@users.noreply.github.com>
2017-11-22 13:12:29 -0600

    Update BusinessList.js

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,8 +5,6 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
 render(){
-  //added this on Sat NOv 18 console.log
-//  console.log(this.props)
   return(
   <div className="BusinessList">
   {this.props.businesses.map(business => {
